256a0fdfc162dc0920839e66c2fc5454486b90aa
Vivy <Tomitaviorel@yahoo.com>
2023-02-09 13:27:14 +0200

    feat: contact page done, installed EmailJS and linked it

--- src/component/contact/contact.jsx
+++ src/component/contact/contact.jsx
@@ -1,7 +1,91 @@
 import './contact.css';
+import { HiOutlineMail } from 'react-icons/hi';
+import { BsWhatsapp } from 'react-icons/bs';
+import { RiMessengerLine } from 'react-icons/ri';
+import { useRef } from 'react';
+import emailjs from 'emailjs-com';
 
 const Contact = () => {
-  return <section id='contact'>Contact</section>;
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        'service_kelirxk',
+        'template_r2u2jqp',
+        form.current,
+        'TTydRH_qDDU6nah9p'
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        },
+        e.target.reset()
+      );
+  };
+
+  return (
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className='container contact__container'>
+        <div className='contact__options'>
+          <article className='contact__option'>
+            <HiOutlineMail className='contact__option-icon' />
+            <h4>Email</h4>
+            <h5>Send me an Email</h5>
+            <a href='thisistheemail@email.com' target='_blank'>
+              Send Message
+            </a>
+          </article>
+          <article className='contact__option'>
+            <RiMessengerLine className='contact__option-icon' />
+            <h4>Messenger</h4>
+            <h5>Message me on FB Messenger</h5>
+            <a href='https://m.me/hisId' target='_blank'>
+              Send Message
+            </a>
+          </article>
+          <article className='contact__option'>
+            <BsWhatsapp className='contact__option-icon' />
+            <h4>WhatsApp</h4>
+            <h5>Give me a quick Message here</h5>
+            <a
+              href='https://api.whatsapp.com/send?phone=+123456789'
+              target='_blank'
+            >
+              Send Message
+            </a>
+          </article>
+        </div>
+        <form ref={form} onSubmit={sendEmail}>
+          <input
+            type='text'
+            name='name'
+            placeholder='Your Full Name'
+            required
+          />
+          <input type='email' name='email' placeholder='Your Email' required />
+          <textarea
+            name='message'
+            id=''
+            rows='7'
+            placeholder='Your Message'
+            required
+          ></textarea>
+          <button type='submit' className='btn btn-primary'>
+            Send Message
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
